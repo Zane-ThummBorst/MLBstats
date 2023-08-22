680a99037ecbea648290bcc02ca430840cbbadfc
Zane_ThummBorst <thummborstz@gmail.com>
2023-08-22 14:35:07 -0400

    base functions (no error checking)

--- example/mlb/back.py
+++ example/mlb/back.py
@@ -1,5 +1,6 @@
 import statsapi as sa
 import django
+
 # sets of all possible stats for leader functions (hitting and pitching)
 # May be useful if combining data
 # hitting_stats = {"era", "homeRuns", "atBats", "runs", "hits", "doubles", "triples", "rbi", "walks", "strikeouts", "stolenBases",
@@ -9,7 +10,7 @@ import django
 #                   "inningsPitched", "hits", "runs", "earnedRuns", "homeRuns","hitBatsman", "walks", "strikeouts", "whip", "avg",
 #                   "totalBattersFaced", "numberOfPitches", "pitchesPerInning", "gamesFinished","holds", "intentionalWalks",
 #                   "wildPitches", "balks", "groundIntoDoublePlay", "StolenBases", "caughtStealing", "pickoffs"}
-abbreviations ={
+abbreviations = {
     "gamesPlayed": "GP",
     "groundOuts": "GO",
     "airOuts": "AO",
@@ -44,32 +45,104 @@ abbreviations ={
     "atBatsPerHomeRun": "AB/HR"
 }
 
+
+# Hitting stats
 def playerInfo(name):
     # Think about an edge case where there are two players with the same name????
     for player in sa.lookup_player(name):
-        #print('Full name: {}, Position: {}'.format(player['fullName'], player['primaryPosition']['abbreviation']))
         d = sa.player_stat_data(player.get('id'), 'hitting', 'season')
         d = d.get('stats')
         stats = d[0].get('stats')
-        # for key in stats:
-
         print(stats)
         return [stats, abbreviations]
+
+
+# Pitching stats
+def playerInfoP(name):
+    # Think about an edge case where there are two players with the same name????
+    for player in sa.lookup_player(name):
+        d = sa.player_stat_data(player.get('id'), 'pitching', 'season')
+        d = d.get('stats')
+        stats = d[0].get('stats')
+        return [stats, abbreviations]
+
 
 def lookup(name):
     player_data = sa.lookup_player(name)
     return player_data[0]
+
 
 def team(id):
     team = sa.lookup_team(id)
     print(team)
     return team[0].get('name')
 
-# x = 0
-# for player in sa.lookup_player("Aaron Judge"):
-#     print(player)
-#
-# # print(sa.get('player',)
+
+'''
+Functions to add:
+    (NOTE: not all api calls return nicely; some return just as one big string which requires some parsing. Save these for last).
+    get pitchers and pitching stats (done)
+    linescores (hits runs for game on specific data)
+    gamepace stats for a given year back to 1999
+    get is an advanced use of the api, try to figure out how to use
+    last_game gets data from most recent game
+    stat leader function
+    roster function
+    schedule function
+    standing function
+'''
+
+# get boxscore given a date and a team (can be either 1 or 2 teams)
+'''
+how to tackle:
+info is coming back in an ok but length format
+work on each part one at a time(each piece of info should be treated as its own container
+work first on: teamInfo, totals
+'''
 
 
+def getBoxScore(date, team_id):
+    # edge_cases (double header) OR no games
+    gid = sa.schedule(date=date, team=143)[0]['game_id']
+    box = sa.boxscore_data(gamePk=gid)
+    return box
+
+def categories():
+    arr = []
+    leaderCategories = sa.get(endpoint='meta', params={'type': 'leagueLeaderTypes'})
+    for e in leaderCategories:
+        arr.append(e['displayName'])
+    return arr
+
+def statLeaders(category, group, season):
+    leaders = sa.league_leader_data(leaderCategories=category, limit=10, statGroup=group, season=season)
+    return leaders
+
+def season_timeline():
+    data = sa.latest_season()
+    print(data)
+
+def standings():
+    stand = sa.standings_data(leagueId="103", division="all", include_wildcard=True, season=None, standingsTypes=None, date=None)
+    return stand
+
+def roster():
+    r = sa.roster(143)
+    result =[]
+    arr = r.split('\n')
+    for e in arr:
+        if e == '':
+            continue
+        e = (" ".join(e.split())).split(' ')
+        name = ""
+        for i in range(2, len(e)):
+            name += e[i] + ' '
+        result.append(
+            {
+                'number': e[0],
+                'position': e[1],
+                'name': name.rstrip()
+            }
+        )
+    return result
 
