1dd39d9c89d0717555571543afa9120f6d7e95ed
Zane_ThummBorst <thummborstz@gmail.com>
2023-08-22 11:28:39 -0400

    users can register (no styling)

--- example/members/urls.py
+++ example/members/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login_user', views.login_user, name="login2"),
     path('logout_user', views.logout_user, name="logout2"),
+    path('register_user', views.register_user, name="register_user"),
 ]
--- example/members/views.py
+++ example/members/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 # Create your views here.
@@ -23,3 +24,19 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('search')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # message mayhaps later
+            return redirect('search')
+    else:
+        form = UserCreationForm()
+    return render(request, "authenticate/register_user.html", {'form': form})
